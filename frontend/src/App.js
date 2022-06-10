feccda9ac686f221ed624f81dc6a712fb0892f1c
Maria Cowdrey <mariacowdrey@yahoo.com>
2022-06-10 15:49:25 -0700

    add bootstrap styles

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,20 @@
-
+import React from 'react'
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
     <>
-  <h1>Beauty Shop</h1>
+      <Header />
+      <main>
+        <Container>
+          <h1>Beauty Shop</h1>
+        </Container>
+      </main>
+      <Footer />
     </>
-  );
+  )
 }
 
-export default App;
+export default App
