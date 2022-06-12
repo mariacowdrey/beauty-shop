d51e1ee1b3693e26a63833540adf09b1d0d0792b
Maria Cowdrey <mariacowdrey@yahoo.com>
2022-06-12 12:19:14 -0700

    add products

---
+++ frontend/src/products.js
@@ -0,0 +1,82 @@
+const products = [
+  {
+    _id: '1',
+    name: 'Roller Lash Mascara',
+    image: '/images/benefit-rollerlash.webp',
+    description:
+      'A mascara inspired by hair rollers, this super-curling and lifting mascara gives a sexy, wide-eyed look. Its patent-pending Hook ‘n’ Roll™ brush features tiny custom-designed hooks to catch, lift, and curl even short and straight lashes.',
+    brand: 'Benefit Cosmetics',
+    category: 'Makeup',
+    price: 27.0,
+    countInStock: 100,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    _id: '2',
+    name: 'Laguna Bronzing Cream',
+    image: '/images/laguna-cream.webp',
+    description:
+      'A decadently creamy bronzing formula that is inspired by the bestselling shade Laguna.',
+    brand: 'NARS',
+    category: 'Makeup',
+    price: 38.0,
+    countInStock: 150,
+    rating: 5.0,
+    numReviews: 8,
+  },
+  {
+    _id: '3',
+    name: 'Retro Eyeshadow Palette',
+    image: '/images/natasha-palette.webp',
+    description:
+      'A midi-sized, must-have eyeshadow palette featuring 15 romantic burgundy and mauve shades combined with gray-browns, taupes, dusty roses, and vintage pinks. Perfect to create romantic, retro-inspired looks.',
+    brand: 'Natasha Denona',
+    category: 'Makeup',
+    price: 65.0,
+    countInStock: 100,
+    rating: 4.2,
+    numReviews: 12,
+  },
+  {
+    _id: '4',
+    name: 'Bridgerton SatinAllure Lipstick',
+    image: '/images/pat-mcgrath-liptsick.webp',
+    description:
+      'A modernized, satin lipstick that veils lips in ravishing Regency-inspired color and a luminous, dewy finish.',
+    brand: 'PAT McGRATH LABS',
+    category: 'Makeup',
+    price: 28.0,
+    countInStock: 100,
+    rating: 4.0,
+    numReviews: 12,
+  },
+  {
+    _id: '5',
+    name: 'Limited Edition Supersonic Hair Dryer',
+    image: '/images/dyson-dryer.webp',
+    description:
+      'A new, limited gift edition Dyson Supersonic™ hair dryer in fuchsia and nickel, with a Dyson-designed Prussian blue presentation case.',
+    brand: 'Dyson',
+    category: 'Hair',
+    price: 429.0,
+    countInStock: 75,
+    rating: 4.1,
+    numReviews: 10,
+  },
+  {
+    _id: '6',
+    name: 'Mini PLAY Everyday Lotion SPF 50',
+    image: '/images/supergoop.webp',
+    description:
+      'A fast-absorbing, non-greasy, water- and sweat-resistant, reef-safe, and broad-spectrum SPF 50 lotion for the face and body.',
+    brand: 'Supergoop!',
+    category: 'Skincare',
+    price: 22.0,
+    countInStock: 75,
+    rating: 4.0,
+    numReviews: 12,
+  },
+]
+
+export default products
